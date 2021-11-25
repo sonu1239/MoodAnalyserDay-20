50ec4a688a1367bbd0eff832de2cf255f03cfff4
sonu1239 <ss316742@gmail.com>
2021-11-25 16:52:30 +0530

    Adding mood check to remote

--- MoodTestCase/MoodTestCase.cs
+++ MoodTestCase/MoodTestCase.cs
@@ -1,109 +1,57 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyser01_Core;
 
-using MoodAnalyser;
-
-namespace moodAnalysisTest
+namespace MoodAnalyser01Test_Core
 {
     [TestClass]
-    public class MoodTestCase
+    public class UnitTest1
     {
-        //[TestMethod]
-        //public void TestMethod1()
-        //{
-        ////TEST CASE-3.1
-
-        ////Arrange
-        //string message = null;
-        //string expected = "Message Should Not Be Null";
-        //string actual = null;
-
-        //try
-        //{
-        //    //Act
-        //    moodAnalysis analyser = new moodAnalysis(message);
-        //    actual = analyser.AnalyseMood();
-        //}
-        //catch (MoodAnalyserCustomException ex)
-        //{
-        //    //Assert
-        //    Assert.AreEqual(expected, ex.Message);
-        //}
-
-
-        //}
-
-        //[TestMethod]
-        //public void getCustomNullException()
-        //{
-        ////TEST CASE-3.2
-        //    //Arrange
-        //    string expected = "Message Should Not Be Empty";
-        //    moodAnalysis modeAnalyzer = new moodAnalysis(string.Empty);
-
-        //    try
-        //    {
-        //        //Act
-        //        string actual = modeAnalyzer.AnalyseMood();
-
-        //    }
-        //    catch (MoodAnalyserCustomException ex)
-        //    {
-
-        //        //Assert
-        //        Assert.AreEqual(expected, ex.Message);
-        //    }
-
-        //}
-
-        //[TestMethod]
-        ////Test Case 4.1 given mood Analyse class name should return mood analyser object.
-
-        //public void GivenMoodAnalyseClassName_Should_return_MoodAnalyseObject()
-        //{
-        //    string message = null;
-        //    object expected = new moodAnalysis(message);
-        //    object obj = MoodAnalyserFactory.CreateMoodAnalyse("MoodAnalyser01_Core.MoodAnalyser", "MoodAnalyser");
-        //    expected.Equals(obj);
-        //}
 
         [TestMethod]
-        //Test Case 4.2 Given mood Analyse wrong class name should return exception stating no such class name exist 
+        //Test Case 5.1 given mood Analyse class name should return mood analyser object with parameterised constructor
 
-        public void GivenMoodAnalyseWrongClassName_Should_return_MoodAnalyseObjectException_Message()
+        public void Given_ParameterisedConstructor_MoodAnalyseClassName_Should_return_MoodAnalyseObject()
+        {
+            object expected = new MoodAnalyser("HAPPY");
+            object obj = MoodAnalyserFactory.CreateMoodAnalyserParameterisedConstructor("MoodAnalyser01_Core.MoodAnalyser", "MoodAnalyser");
+            expected.Equals(obj);
+        }
+
+
+        [TestMethod]
+        //Test Case 5.2 Given mood Analyse wrong class name should return exception stating no such class name exist 
+
+        public void Given_ParameterisedConstructor_WrongClassName_Should_return_MoodAnalyseObjectException_Message()
         {
             try
             {
-                string message = null;
-                object expected = new moodAnalysis(message);
-                object obj = MoodAnalyserFactory.CreateMoodAnalyse("MoodAnalyser01_Core.MoodAnalyserWrong", "MoodAnalyserWrong");
+                object expected = new MoodAnalyser("HAPPY");
+                object obj = MoodAnalyserFactory.CreateMoodAnalyserParameterisedConstructor("MoodAnalyser01_Core.MoodAnalyserWrong", "MoodAnalyser");
                 expected.Equals(obj);
             }
 
-            catch (MoodAnalyserCustomException e)
+            catch (CustomMoodAnException e)
             {
                 Assert.AreEqual("Class not found", e.Message);
             }
         }
 
-        [TestMethod]
-        //Test Case 4.3 Given wrong constructor name should return simproper message in exception  
 
-        public void GivenMoodAnalyseWrongConstructor_Should_return_MoodAnalyseObjectException_Message()
+        //Test Case 5.3 Given mood Analyse wrong constructor name should return exception stating no such consrtructor exist 
+        [TestMethod]
+        public void Given_ParameterisedConstructor_WrongConstrcutorName_Should_return_MoodAnalyseObjectException_Message()
         {
             try
             {
-                string message = null;
-                object expected = new moodAnalysis(message);
-                object obj = MoodAnalyserFactory.CreateMoodAnalyse("MoodAnalyser01_Core.MoodAnalyser", "MoodAnalyserWrong");
+                object expected = new MoodAnalyser("HAPPY");
+                object obj = MoodAnalyserFactory.CreateMoodAnalyserParameterisedConstructor("MoodAnalyser01_Core.MoodAnalyser", "MoodAnalyserWrong");
                 expected.Equals(obj);
             }
 
-            catch (MoodAnalyserCustomException e)
+            catch (CustomMoodAnException e)
             {
                 Assert.AreEqual("Constructor not found", e.Message);
             }
         }
-
     }
-
 }
