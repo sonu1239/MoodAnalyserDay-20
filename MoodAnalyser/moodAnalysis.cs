ea2f9c11b2c33586203b3fd50d653cd03f395167
sonu1239 <ss316742@gmail.com>
2021-11-25 16:26:08 +0530

    Adding mood analyser

--- MoodAnalyser/moodAnalysis.cs
+++ MoodAnalyser/moodAnalysis.cs
@@ -4,46 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MoodAnalyser01_Core
+namespace MoodAnalyser
 {
-    public class MoodAnalyser
+    public class moodAnalysis
     {
-        private string message;        //the private message field of the mood analyser class
+        public string message;
 
-        public MoodAnalyser(string message)        //initialising the parameterised constructor
+        public moodAnalysis(string message)
         {
             this.message = message;
         }
-
-        public MoodAnalyser()                //initialising the default constructor
+        public string AnalyseMood(string message)
         {
-            this.message = null;
-        }
-
-        public string AnalyseMood()                  //declaring the analyse mood method
-        {
-            try                                      //the try and catch block is for exception handling
+            if (message.ToLower().Contains("sad"))
             {
-                //this is the custom exception that we declared for checking empty messages. exception type is an enum followed by the message.
-                if (this.message.Equals(string.Empty))
-                {
-                    throw new CustomMoodAnException(CustomMoodAnException.ExceptionType.EMPTY_MESSAGE, "Mood should not be Empty");
-                }
-
-                if (this.message.ToLower().Contains("sad"))
-                {
-                    return "SAD";
-                }
-                else
-                {
-                    return "HAPPY";
-                }
+                return "SAD";
             }
-
-            catch (NullReferenceException)                  //this shows that it should not be null. NullREferenceException is a predefined exception class
-            {
-                throw new CustomMoodAnException(CustomMoodAnException.ExceptionType.NULL_VALUE, "Mood can not be null");
-            }
+            else
+                return "HAPPY";
         }
     }
 }
